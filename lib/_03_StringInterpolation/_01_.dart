1e6d87aa45fe6a68fa0caa5d840ba0cd6b702e48
abhishekmoral <abhisheksinghmoral@gmail.com>
2026-01-30 11:04:23 +0530

    String interpolation complete

---
+++ lib/_03_StringInterpolation/_01_.dart
@@ -0,0 +1,17 @@
+void main (){
+  String str1 = "I am learning ";
+  String str2 = "Dart";
+  // Using  +operator.
+  String str3 = str1 + str2;
+  print(str3);
+  // Using $ operator.
+  String str4 = '$str1$str2';
+  print(str4);
+  // Using ${}.
+  print("Hello , ${str1}${str2}");
+  // other methods
+  var num = 56;
+  print("Total marks : " +num.toString());
+
+
+}
