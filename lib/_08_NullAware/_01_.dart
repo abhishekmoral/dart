d4f4843b567db2e26ca0b7f31dda7e2f23787b49
abhishekmoral <abhisheksinghmoral@gmail.com>
2026-01-30 22:39:00 +0530

    complete

---
+++ lib/_08_NullAware/_01_.dart
@@ -0,0 +1,7 @@
+// only used with nullable variables
+void main (){
+  int? value;
+  value??= 56; // if variable contains null
+  print(value);
+
+}
---
+++ lib/_08_NullAware/_02_.dart
@@ -0,0 +1,5 @@
+late int testValue ; // Global non-nullable variable.
+void main(){
+  testValue=75;
+  print(testValue);
+}
