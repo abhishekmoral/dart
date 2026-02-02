143f2bb382425c91ed466f7b0d1cde5b2d360b13
abhishekmoral <abhisheksinghmoral@gmail.com>
2026-02-02 09:20:04 +0530

    Assert complete

---
+++ lib/_01_.dart
@@ -0,0 +1,11 @@
+// Purpose?
+// to disrupt normal execution if a boolean condition is false.
+void main (){
+  var text = "Abhishek Moral";
+  assert(text!= null);
+  var length = text.length;
+  assert(length > 0);
+  var urlString = "https://google.com";
+  assert(urlString.startsWith("htts://"),"Enter Proper url");
+
+}
