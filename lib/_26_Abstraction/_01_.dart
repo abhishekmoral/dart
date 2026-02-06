47dcbe077e52105c480d7abd3db7f515c412b8bb
abhishekmoral <abhisheksinghmoral@gmail.com>
2026-02-06 17:57:54 +0530

    complete

---
+++ lib/_26_Abstraction/_01_.dart
@@ -0,0 +1,32 @@
+void main() {
+  var doctor = new Doctor();
+  doctor.getEmployeeType();
+  var paraMedic = new ParaMedic();
+  paraMedic.getEmployeeType();
+  var nurse = new Nurse();
+  nurse.getEmployeeType();
+}
+
+abstract class Employee {
+  void getEmployeeType(); // Abstract Method.
+}
+
+class Doctor extends Employee {
+  @override
+  void getEmployeeType() {
+    print(" Employee Type : Doctor");
+  }
+
+}
+class ParaMedic extends Employee {
+  @override
+  void getEmployeeType() {
+    print(" Employee Type : ParaMedic");
+  }
+}
+class Nurse extends Employee {
+  @override
+  void getEmployeeType() {
+    print(" Employee Type : Nurse");
+  }
+
